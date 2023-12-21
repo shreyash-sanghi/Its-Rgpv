085daabaed9d3ace452655fbc90d1d79807ea5a7
shreyash-sanghi <148464006+shreyash-sanghi@users.noreply.github.com>
2023-12-21 08:15:32 +0530

    Update by remove localhost from cors

--- Express/src/app.js
+++ Express/src/app.js
@@ -17,7 +17,7 @@ const path = require("path");
 //React Cors
 // app.use(cors(
 //   {
-//     origin:["http://localhost:5173"],
+//     origin:["http://its-rgpv-2m34.vercel.app"],
 //     credentials:true,
 //     optionsSuccessStatus:200
 //   }
