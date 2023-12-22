1c053dcc0f210fd90aa3d026c1c4ed596c789bb9
shreyash jain <sheryash123jain@gmail.com>
2023-12-22 19:09:28 +0530

    mogo_uri added, entry point correction

--- Express/src/DB/RegisterData.js
+++ Express/src/DB/RegisterData.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.connect(`mongodb+srv://shreyashjain:Bittu@8085@shreyashjain.dhe7mgt.mongodb.net/${maindatafolder}?retryWrites=true&w=majority`)
+mongoose.connect(process.env.MONGO_URI)
 .then(()=>{console.log(" Data-Base connection sucessfully....")})
 .catch((e)=>{console.log(e)})
