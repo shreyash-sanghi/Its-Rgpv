24dcc8d585ec37dafc4915df62e87ddf62724ee4
shreyash jain <sheryash123jain@gmail.com>
2023-12-23 12:19:06 +0530

    app.jsx

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,5 +1,6 @@
 import React,{useState,useEffect} from 'react'
 import Navbar from './components/MiainFile'
+import axios from 'axios';
 // import Eventadd from '../../React/EventX/src/components/Eventadd'
 
 const App = () => {
