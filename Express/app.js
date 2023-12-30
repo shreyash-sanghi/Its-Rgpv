775f5159e3e55d56984a4e141dd4646943b8e660
shreyash jain <sheryash123jain@gmail.com>
2023-12-30 09:43:59 +0530

    remove header form app

--- Express/app.js
+++ Express/app.js
@@ -26,14 +26,14 @@ app.use(cors(
 
 
 //Tak Data Function
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', "https://its-rgpv.vercel.app"); 
-  res.setHeader('Access-Control-Allow-Credentials', "true");
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+// app.use(function(req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', "https://its-rgpv.vercel.app"); 
+//   res.setHeader('Access-Control-Allow-Credentials', "true");
+//   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
-  next();
-});
+//   next();
+// });
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'))
