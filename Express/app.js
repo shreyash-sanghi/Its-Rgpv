dea738b3bab3b88bb02a6bdc374b5489d248e41d
shreyash jain <sheryash123jain@gmail.com>
2023-12-30 19:22:23 +0530

    origin

--- Express/app.js
+++ Express/app.js
@@ -27,7 +27,7 @@ app.use(cors(
 
 //Tak Data Function
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "https://its-rgpv.vercel.app"); 
+  res.header('Access-Control-Allow-Origin', "http://localhost:5173"); 
   res.header('Access-Control-Allow-Credentials', "true");
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
